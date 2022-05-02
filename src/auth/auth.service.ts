599118984cb66f075432f361e3b4327bb1bd51c8
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-02 17:31:51 +0000

    some-update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterUserDto } from 'src/DTO/registerUser.dto';
 import { UserEntity } from 'src/Entity/user.entity';
@@ -9,47 +14,53 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>, private jwt: JwtService ) {}
+  constructor(
+    @InjectRepository(UserEntity) private repo: Repository<UserEntity>,
+    private jwt: JwtService,
+  ) {}
 
+  async registerUser(registerDto: RegisterUserDto) {
+    const { username, password } = registerDto;
+    const hashed = await bcrypt.hash(password, 12);
+    const salt = await bcrypt.getSalt(hashed);
 
-    async registerUser(registerDto:RegisterUserDto) {
-        const {username, password} = registerDto;
-        const hashed = await bcrypt.hash(password,12);
-        const salt = await bcrypt.getSalt(hashed);
-        console.log(hashed, salt);
+    const foundUser = await this.repo.findOne({ username });
 
-        const user = new UserEntity();
-        user.username = username;
-        user.password = hashed;
-        user.salt = salt;
+    if (foundUser) {
+      throw new BadRequestException('User already exists!');
+    } else {
+      console.log(hashed, salt);
 
-        this.repo.create(user);
-        try {
-            
-            return await this.repo.save(user);
+      const user = new UserEntity();
+      user.username = username;
+      user.password = hashed;
+      user.salt = salt;
 
-        } catch (error) {
-            
-            throw new InternalServerErrorException('Something went wrong !');
-
-        }
-        
+      this.repo.create(user);
+      try {
+        return await this.repo.save(user);
+      } catch (error) {
+        throw new InternalServerErrorException('Something went wrong !');
+      }
     }
+  }
 
-    async loginUser(logUserDto: LogUserDto) {
-        const {username, password} = logUserDto;
-        const user = await this.repo.findOne({username});
-        if(!user) {
-            throw new  UnauthorizedException('Invalid Operation.');
-        }
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (isPasswordMatch) {
-            const jwtPayload = {username};
-            const jwtToken = await this.jwt.signAsync(jwtPayload, {expiresIn: '1d', algorithm:'HS512'});
-            return {token: jwtToken};
-        }
-        else {
-            throw new  UnauthorizedException('Invalid Operation.');
-        }
+  async loginUser(logUserDto: LogUserDto) {
+    const { username, password } = logUserDto;
+    const user = await this.repo.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException('Invalid Operation.');
     }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (isPasswordMatch) {
+      const jwtPayload = { username };
+      const jwtToken = await this.jwt.signAsync(jwtPayload, {
+        expiresIn: '1d',
+        algorithm: 'HS512',
+      });
+      return { token: jwtToken };
+    } else {
+      throw new UnauthorizedException('Invalid Operation.');
+    }
+  }
 }
